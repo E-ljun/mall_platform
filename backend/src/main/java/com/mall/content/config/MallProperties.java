850ec971bf7b65d4c82069ebac779b47dbd9061a
E-ljun <linsjun@126.com>
2026-06-28 12:06:54 +0800

    fix: PDF export - switch from OTF Noto fonts to TrueType WenQuanYi fonts

--- backend/src/main/java/com/mall/content/config/MallProperties.java
+++ backend/src/main/java/com/mall/content/config/MallProperties.java
@@ -68,12 +68,12 @@ public class MallProperties {
 
     @Data
     public static class Pdf {
-        private String fontPaths = "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc,"
+        private String fontPaths = "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc,"
+                + "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc,"
                 + "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc,"
-                + "/usr/share/fonts/noto-cjk/NotoSansCJK-Regular.ttc,"
                 + "C:/Windows/Fonts/msyh.ttc,"
                 + "C:/Windows/Fonts/simsun.ttc";
-        private String fontFamily = "Noto Sans CJK SC";
+        private String fontFamily = "WenQuanYi Micro Hei";
     }
 
     @Data
